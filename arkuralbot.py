8a787dc7e83a934d85c046cd1c7fbc9f6f0d7b60
muratthedoctor <kiritotr@gmail.com>
2019-05-30 01:38:13 +0300

    Update arkuralbot.py

--- arkuralbot.py
+++ arkuralbot.py
@@ -79,5 +79,5 @@ client = MyClient()
 client.run('token')
 """
 
-#client.run("MzMzMjQxMTM0MDIzMTgwMjk5.XOmn9g.VkQy2-P2zQBogmhMMv36ID7ra2I")
-client.run(str(os.environ.get('BOT_TOKEN')))
+client.run("BOT_TOKEN")
+#client.run(str(os.environ.get('MzMzMjQxMTM0MDIzMTgwMjk5.XOmn9g.VkQy2-P2zQBogmhMMv36ID7ra2I')))
