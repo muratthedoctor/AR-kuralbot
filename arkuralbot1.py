f4e000e5174519285a52af01e6e6a88b4bdc3fa6
muratthedoctor <kiritotr@gmail.com>
2021-08-20 17:26:55 +0300

    Create arkuralbot1.py
    sıfırdan arkuralbot.py yaratıyorum. DAHA BITMEDI

---
+++ arkuralbot1.py
@@ -0,0 +1,11 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+from discord.utils import get
+import asyncio
+import time
+import csv
+import random
+import os
+
+client = discord.Client()
