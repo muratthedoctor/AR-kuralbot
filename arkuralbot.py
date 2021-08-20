9fcdb5eb987a67c07fdddb58b1f687e084687dbc
muratthedoctor <kiritotr@gmail.com>
2021-08-20 22:58:02 +0300

    Update arkuralbot.py

--- arkuralbot.py
+++ arkuralbot.py
@@ -17,7 +17,7 @@ async def on_ready():
   print(client.user.name)
   print('------')
   
-@client.evet
+@client.event
 async def on_message(message):
   if message.author == client.user:
     return
